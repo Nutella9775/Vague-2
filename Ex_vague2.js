029dcd1e459b930e238394dfb1bfead650b22371
Nutella9775 <adrienbarbet7@gmail.com>
2025-09-15 21:22:52 +0200

    feat: add ex1

--- Ex_vague2.js
+++ Ex_vague2.js
@@ -1,2 +1,15 @@
 //ex 1 
+/* Créez un objet personne avec les propriétés vous concernant suivantes : prenom, nom, age, ville
+Affichez les propriétés prenom, nom, age et ville de l'objet personne dans des console.log() séparés. */
 
+let personne = {
+    prenom: "Adrien",
+    nom: "Barbet",
+    age: 19,
+    ville: "Caen",
+};
+
+console.log("Prenom : " +personne.prenom);
+console.log("Nom : " +personne.nom);
+console.log("Age : " +personne.age,"ans");
+console.log("Ville : " +personne.ville);
