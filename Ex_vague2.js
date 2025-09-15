c53d0a887ef0bdcb2223d9fe09972c0568a816d0
Nutella9775 <adrienbarbet7@gmail.com>
2025-09-15 21:35:06 +0200

    feat: add ex2

--- Ex_vague2.js
+++ Ex_vague2.js
@@ -13,3 +13,26 @@ console.log("Prenom : " +personne.prenom);
 console.log("Nom : " +personne.nom);
 console.log("Age : " +personne.age,"ans");
 console.log("Ville : " +personne.ville);
+
+console.log("_________________________________________________________________________");
+console.log("\n");
+//ex2
+/*Créez un tableau personnes contenant les objets suivants :
+- {prenom: "John", nom: "Doe", age: 25, ville: "Rouen"}
+- {prenom: "Jane", nom: "Doe", age: 30, ville: "Paris"}
+- {prenom: "Jim", nom: "Doe", age: 35, ville: "Caen"}
+Affichez le tableau personnes dans la console avec log() et table().*/
+
+let personnes = [
+
+    { prenom:"John",nom: "Doe", age:"25", ville:"Rouen"},
+    { prenom:"Jan",nom: "Doe", age:"30", ville:"Paris"},
+    { prenom:"Jim",nom: "Doe", age:"35", ville:"Caen"},
+
+];
+
+console.log("Affichage avec console.log() :");
+console.log(personnes);
+
+console.log("Affichage avec console.table() :");
+console.table(personnes);
